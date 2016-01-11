f9cbc9aa7d197bb1a8b64cfa46fb2c8b4edbdee5
Mitchell Arij Cox <mitch@enox.co.za>
2016-01-11 17:40:34 +0200

    Added automatic amplitude modulation on the hologram if p ~= 0 or if multiple l values are used.

--- Holography/hologram.m
+++ Holography/hologram.m
@@ -1,14 +1,15 @@
 function [] = hologram(N, k, theta, p, l, w, fs)
 % Generates Gaussian beam hologram
 % N(1), N(2) : number of pixels along each dimension
-% m : number of grids
-% k : grid orientation
-% p : radial Order
-% l : topological charge
+% k : ??
+% theta: grid orientation
+% p : radial Order. if p ~= 0 then amplitude is used too.
+% l : topological charge. Can be a matrix [1 2; 5 19] will lay them in a
+% square. This implies amplitude modulation.
 % w : radius
 % fs: the screen where to display fullscreen. 0 is a window. ALT-TAB to
 % exit fullscreen.
-% Example: hologram([1920 1080], 100, 45, 0, 1, 10)
+% Example: hologram([1920 1080], 100, 45, 0, 1, 10, 1)
 grid=size(l);
 N([1 2])=fliplr(N);
 points=N./grid;
@@ -20,11 +21,19 @@ x=linspace(-range(1), range(1), points(1));
 y=linspace(-range(2), range(2), points(2));
 [yy,xx]=meshgrid(y,x);
 
+amp = 0;
+if p ~= 0 
+    amp = 1;
+end
+if max(size(l)) > 1
+    amp = 1;
+end
+
 A(points(1), grid(1), points(2), grid(2))=uint8(0);
 for i=1:grid(1)
     for j=1:grid(2)
         E=LGBeam(p(i,j), l(i,j), w(i,j), xx, yy);
-        A(:,i,:,j)=planeWave(E, xx, yy, k, theta, 0);
+        A(:,i,:,j)=planeWave(E, xx, yy, k, theta, amp);
     end
 end
 A=reshape(A, N);
@@ -32,7 +41,8 @@ A=reshape(A, N);
 if fs == 0
     figure(1); imshow(A,'Border','tight','InitialMagnification','fit'); truesize(1);
     map=gray(256); colormap(map); 
-    %imwrite(Hol, map, 'Test.png');
+    file = strcat('holograms\hologram-l',int2str(l),'p',int2str(p),'w',int2str(w),'k',int2str(k),'theta',int2str(theta),'.png');
+    imwrite(A, map, file);
 else
     fullscreen(A,fs);
 end
