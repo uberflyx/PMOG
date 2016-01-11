0c1d41a5a3050a9e005c18550d1906594c6fe623
Mitchell Arij Cox <mitch@enox.co.za>
2016-01-11 18:07:27 +0200

    Enabled animation.m for a vector of l and p values with arbitrary delay.

--- Holography/animation.m
+++ Holography/animation.m
@@ -1,8 +1,17 @@
-function [] = animation(Nx, Ny, m, k, p, w)
-for i=0:100
-    l=mod(i,11)-5;
-    hologram(Nx,Ny,m,k,p,l,w);
-    pause(.4);
-end
+function [] = animation(N, k, theta, w, pvec, lvec, delay, fs)
+%%%
+% Animate through all the l values in lvec, 
+% and also all the p values in pvec,
+% displaying each hologram for delay seconds. 
+% All parameters are the same as hologram.m
+% Examples: 
+% animation([1920 1080], 100, 45, 0.2, 0, [1 2 3 4 5], 0.5, 0)
+% animation([1920 1080], 100, 45, 0.2, [1 2], [1 2 3 4 5], 0.5, 1)
+%%%
+for j = 1:size(pvec,2)
+    for i=1:size(lvec,2)
+        hologram(N, k, theta, pvec(j), lvec(i), w, fs);
+        pause(delay);
+    end
 end
 
