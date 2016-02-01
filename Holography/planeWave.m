92ffe0702ab15b86c39d04377d84048e60be03e6
Mitchell Arij Cox <mitch@enox.co.za>
2016-02-01 13:54:10 +0200

    blazed grating is not tuned to wavelength...

--- Holography/planeWave.m
+++ Holography/planeWave.m
@@ -7,12 +7,18 @@ function [H] = planeWave(E, xx, yy, k, theta, mode)
 theta=pi/180*theta;
 plane=sin(theta)*xx+cos(theta)*yy;
 phase=angle(E);
+
+%Blazed Grating:
+H=mod(phase+k*plane+pi, 2*pi)-pi;
+
+%Cosine Grating:
+%H=sin(phase+k*plane+pi);
+
 if mode 
-    I=E.*conj(E);
-    H=I.*(mod(phase+k*plane+pi, 2*pi)-pi);
-else
-    H=mod(phase+k*plane+pi, 2*pi)-pi;
+    I=E .* conj(E);
+    H=I .* H;   
 end
+
 a=min(H(:)); b=max(H(:));
 H=uint8(255*(H-a)/(b-a));
 end
