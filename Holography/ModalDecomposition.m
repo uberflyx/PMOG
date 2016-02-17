a0979383bca3dc2a81a08407ca6c63dfda0bb612
Mitch Cox <mitch@enox.co.za>
2016-02-17 09:32:16 +0200

    modal decomposition working nicely.

--- Holography/ModalDecomposition.m
+++ Holography/ModalDecomposition.m
@@ -1,10 +1,12 @@
-function [] = ModalDecomposition(gratingNumber, gratingAngle, beamWidth, pvec, lvec, delay, screen, vid, filePrefix)
+function [md] = ModalDecomposition(gratingNumber, gratingAngle, beamWidth, pvec, lvec, prange, lrange, delay, screen, vid, filePrefix)
 %MODALDECOMPOSITION Perform modal decomposition with two halves of an SLM
 %   Saves hologram images in the holograms folder
 %   Script initially sets LG l=1 and -2 for finding the center. 
 %   Displays a find center dialog for user selection.
 %   Script then runs the measurements and asks whether they should be run
 %   again, in case multiple turbulence areas are used.
+%   Tests all p and lvecs agains all p and lranges.
+%   Example: ModalDecomposition(400, 0, 0.15, [0], [1 2 3 4 5],[0],[-5 -4 -3 -2 -1 0 1 2 3 4 5] ,0.2,1,vid,'decom\test')
 
 go = true;
 
@@ -12,7 +14,7 @@ measurementN = 1;
 n = 1;
 
 %Set up
-hologramOAM(gratingNumber, gratingAngle, beamWidth, 0, [1 -2], screen, false, false, [600 300]);
+hologramHalfOAM(gratingNumber, gratingAngle, beamWidth, [0 0], [1 -2], screen, false, false);
 pause(delay);
 
 %display the selection dialog
@@ -21,12 +23,34 @@ fprintf('Center selected at: %s\n', mat2str(center));
 
 fprintf('Running modal decomposition %s...\nMode: LG [l, p]\n', int2str(measurementN));
 
+md = zeros(size(lvec,2), size(lrange,2));
+
 while (go)
 for p = 1:size(pvec,2)
     for l=1:size(lvec,2)
-        [s, se] = hologramOAM(gratingNumber, gratingAngle, beamWidth, [pvec(p) pvec(p)], [lvec(l) -lvec(l)], screen, false, true, [600 300]);
-       
-        fprintf('[%s, %s] ',int2str(lvec(l)), int2str(pvec(p)));
+        %[s, se] = hologramOAM(gratingNumber, gratingAngle, beamWidth, [pvec(p) pvec(p)], [lvec(l) -lvec(l)], screen, false, false);
+        %we set the desired l and p onto the left SLM, and cycle through a
+        %range on the right SLM.
+        fprintf('Generated [%s, %s]:\nTesting: ',int2str(lvec(l)), int2str(pvec(p)));
+        
+        for ptest = 1:size(prange,2)
+            for ltest = 1:size(lrange,2)
+                hologramHalfOAM(gratingNumber, gratingAngle, beamWidth, [pvec(p) prange(ptest)], [lvec(l) lrange(ltest)], screen, false, false);
+                
+                pause(delay); %make sure the SLM has settled
+        
+                %take the picture
+                img = getsnapshot(vid);
+                imwrite(img, strcat(filePrefix, '-MD-slp[', int2str(lvec(l)), '_', int2str(pvec(p)), ']-tlp[',int2str(lrange(ltest)), '_', int2str(prange(ptest)),']-C', mat2str(center), '.png'));
+                %img(center(2), center(1))
+                md(l, ltest) = md(l, ltest) + double(img(center(2), center(1)));
+                
+                pause(delay);
+                
+                fprintf('[%s, %s] ',int2str(lrange(ltest)), int2str(prange(ptest)));
+            end
+        end
+        fprintf('\n');
         
         %Print details nicely (10 per line)
         if mod(n,10) == 0
@@ -35,13 +59,7 @@ for p = 1:size(pvec,2)
         end
         n = n + 1;
         
-        pause(delay); %make sure the SLM has settled
-        
-        %take the picture
-        img = getsnapshot(vid);
-        imwrite(img, strcat(filePrefix, '-MD-l=', int2str(lvec(l)), '-p=', int2str(pvec(p)), '-C=', mat2str(center), '.png'));
-        
-        pause(delay);
+
     end
 end
 
@@ -53,6 +71,8 @@ if strcmpi(a,'y')
 else
     go = false;
 end
+
+md
 
 end
 
