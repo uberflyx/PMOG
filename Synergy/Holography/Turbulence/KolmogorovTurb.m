be240a3d13856f8584ae23f7966608adf36f78ec
Mitchell Arij Cox <mitch@enox.co.za>
2017-07-17 08:26:55 +0200

    performance imporvements to KolmogorovTurb function

--- Synergy/Holography/Turbulence/KolmogorovTurb.m
+++ Synergy/Holography/Turbulence/KolmogorovTurb.m
@@ -4,7 +4,7 @@ function [turbPhaseScreen] = KolmogorovTurb(size_px, r0, D, noTipTilt)
 %Paramters:
 
 %the indeces below are the nollIndex-1 so z1 has noll = 2
-%global zScreens
+persistent zScreens
 
 %tip tilt focus ...
 I = [1.0299 0.5820 0.134 0.111 0.0880 0.0648 0.0587 0.0525 0.0463 0.0401 0.0377 0.0352 0.0328 0.0304 0.0279 0.0267 0.0255 0.0243 0.0232 0.0220];
@@ -15,23 +15,23 @@ if (nargin < 4) || noTipTilt == true
 end
 
 %we cache each of the zernike screens and weight them at the end
-%if isempty(zScreens)
-for i = 1:20
-    zScreens(:,:,i) = GenerateZernike(size_px,i+1,0,2*pi); 
+if isempty(zScreens) || (size(zScreens,1) ~= size_px)
+    zScreens = zeros(size_px,size_px,20);
+    for i = 1:20
+        zScreens(:,:,i) = zScreens(:,:,i) + GenerateZernike(size_px,i+1,0,2*pi);
+    end
 end
-%end
 
 sigmas = sqrt(I .* (D/r0)^(5/3));
-weights = zeros(size_px, size_px, 20);
+%weights = zeros(size_px, size_px, 20);
+w = sigmas .* randn(1,size(sigmas,1));
 
-for i = 1:20
-    %it's faster to add to zeros than multiply ones :P
-    w = (sigmas(1,i) * randn);
-    weights(:,:,i) = weights(:,:,i) + w;
-end
+weights = kron(w, ones(size_px)); %this is now one big 2D matrix
+%split it into a 3D matrix
+weights = reshape(weights, [size_px, 1, 20]);
 
-weighted = weights .* zScreens;
+weighted = zScreens * weights;
 turbPhaseScreen = sum(weighted,3);
-turbPhaseScreen = mod(turbPhaseScreen,2*pi);
+%turbPhaseScreen = mod(turbPhaseScreen,2*pi);
 %turbPhaseScreen = turbPhaseScreen / (2*pi);
 end
