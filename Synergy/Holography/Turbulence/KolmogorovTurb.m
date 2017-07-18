5cea5af8e91f071039421cac7351d85632638a33
Mitch Cox <mitch@enox.co.za>
2017-07-18 13:58:38 +0200

    fixed bugs

--- Synergy/Holography/Turbulence/KolmogorovTurb.m
+++ Synergy/Holography/Turbulence/KolmogorovTurb.m
@@ -9,7 +9,7 @@ persistent zScreens
 %tip tilt focus ...
 I = [1.0299 0.5820 0.134 0.111 0.0880 0.0648 0.0587 0.0525 0.0463 0.0401 0.0377 0.0352 0.0328 0.0304 0.0279 0.0267 0.0255 0.0243 0.0232 0.0220];
 
-if (nargin < 4) || noTipTilt == true
+if (nargin >= 4) && noTipTilt == true
     I(1,1) = 0;
     I(1,2) = 0;
 end
@@ -28,10 +28,9 @@ w = sigmas .* randn(1,size(sigmas,1));
 
 weights = kron(w, ones(size_px)); %this is now one big 2D matrix
 %split it into a 3D matrix
-weights = reshape(weights, [size_px, 1, 20]);
+weights = reshape(weights, [size_px, size_px, 20]);
 
-weighted = zScreens * weights;
+weighted = zScreens .* weights;
 turbPhaseScreen = sum(weighted,3);
-%turbPhaseScreen = mod(turbPhaseScreen,2*pi);
-%turbPhaseScreen = turbPhaseScreen / (2*pi);
+turbPhaseScreen = mod(turbPhaseScreen,2*pi);
 end
